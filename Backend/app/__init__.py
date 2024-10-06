584fa932b204bd405464568ef73bc79587e996d7
Shashi Sugrim <34145834+ShashiSugrim@users.noreply.github.com>
2024-10-06 09:44:26 -0400

    allow cors to send data to frontend

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="My FastAPI Application", description="A barebones FastAPI web application template")
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
 
 from app import views
